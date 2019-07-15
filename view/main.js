ebab3928178fad27369bd73ca528d1d5a1d2bb33
LAM <trinhoanglam214@gmail.com>
2019-07-15 11:39:20 +0700

    update style video

--- view/main.js
+++ view/main.js
@@ -51,17 +51,32 @@ function initCamera() {
   }
 
   if (hasGetUserMedia()) {
+    // const constraints = {
+    //   video: {
+    //     width: {
+    //       min: window.innerWidth
+    //     },
+    //     height: {
+    //       min: window.innerHeight
+    //     },
+    //     facingMode: {
+    //       exact: 'environment'
+    //     }
+    //   }
+    // };
     const constraints = {
       video: {
         width: {
-          min: window.innerWidth
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
         },
         height: {
-          min: window.innerHeight
+          min: 720,
+          ideal: 1080,
+          max: 1440
         },
-        facingMode: {
-          exact: 'environment'
-        }
+        facingMode: 'user'
       }
     };
     const video = document.querySelector('video');
