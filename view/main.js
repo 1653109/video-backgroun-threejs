0a9a61248ba3cc9ce162f5b299776c0dae1023e4
LAM <trinhoanglam214@gmail.com>
2019-07-15 11:39:54 +0700

    update video script

--- view/main.js
+++ view/main.js
@@ -51,34 +51,34 @@ function initCamera() {
   }
 
   if (hasGetUserMedia()) {
-    // const constraints = {
-    //   video: {
-    //     width: {
-    //       min: window.innerWidth
-    //     },
-    //     height: {
-    //       min: window.innerHeight
-    //     },
-    //     facingMode: {
-    //       exact: 'environment'
-    //     }
-    //   }
-    // };
     const constraints = {
       video: {
         width: {
-          min: 1280,
-          ideal: 1920,
-          max: 2560,
+          min: window.innerWidth
         },
         height: {
-          min: 720,
-          ideal: 1080,
-          max: 1440
+          min: window.innerHeight
         },
-        facingMode: 'user'
+        facingMode: {
+          exact: 'environment'
+        }
       }
     };
+    // const constraints = {
+    //   video: {
+    //     width: {
+    //       min: 1280,
+    //       ideal: 1920,
+    //       max: 2560,
+    //     },
+    //     height: {
+    //       min: 720,
+    //       ideal: 1080,
+    //       max: 1440
+    //     },
+    //     facingMode: 'user'
+    //   }
+    // };
     const video = document.querySelector('video');
 
     navigator.mediaDevices
